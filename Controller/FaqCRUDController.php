6dcc9e47f7eef3ede3edf77ea3133e376527d347
Bartosz Malec <b.malec@awaresoft.pl>
2016-09-17 07:52:22 +0200

    Removed unnecessary isBlocked check from CRUDController.

--- Controller/FaqCRUDController.php
+++ Controller/FaqCRUDController.php
@@ -12,34 +12,5 @@ use Awaresoft\Sonata\AdminBundle\Traits\ControllerHelperTrait;
  */
 class FaqCRUDController extends AwaresoftCRUDController
 {
-    use ControllerHelperTrait;
 
-    /**
-     * @inheritdoc
-     */
-    public function preDeleteAction($object)
-    {
-        $message = $this->checkObjectIsBlocked($object, $this->admin);
-
-        return $message;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function batchActionDeleteIsRelevant(array $idx)
-    {
-        $message = null;
-
-        foreach ($idx as $id) {
-            $object = $this->admin->getObject($id);
-            $message = $this->checkObjectIsBlocked($object, $this->admin);
-        }
-
-        if (!$message) {
-            return true;
-        }
-
-        return $message;
-    }
 }
